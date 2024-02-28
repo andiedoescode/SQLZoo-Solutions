c9a574e9723fe9e034ff0a1e7137015a4748ee74
andiedoescode <pang.andrea@gmail.com>
2024-02-27 22:19:35 -0500

    Section 8+ - NSS Tutorial (Numeric Examples)

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ This README file shows the solutions to the [SQLZoo Tutorials](https://www.sqlzo
 6. [JOIN](#6-join)
 7. [More JOIN](#7-more-join)
 8. [Using Null](#8-using-null)
-   - 8+. Numeric Examples
+   - 8+. [Numeric Examples](#8-nss-tutorial-numeric-examples)
 9. Self join
    - 9-. Window function
    - 9+. COVID 19 Example
@@ -1515,6 +1515,7 @@ SELECT name,
             END AS digit
 FROM teacher
 ```
+
 `A: 'four' for Throd`
 
 6. Select the result that would be obtained from the following code:
@@ -1529,4 +1530,97 @@ SELECT name,
 	END
 FROM teacher
 ```
+
 ![Table-A: Computing (Shrivell, Throd, Split), Other (Spiregrain, Cutflower, Deadyawn)](images/usingnull-quiz6.png)
+
+## 8+. NSS Tutorial (Numeric Examples)
+
+![Table for NSS Tutorial](images/nss_tut.png)
+
+National Student Survey 2012
+
+The National Student Survey http://www.thestudentsurvey.com/ is presented to thousands of graduating students in UK Higher Education. The survey asks 22 questions, students can respond with STRONGLY DISAGREE, DISAGREE, NEUTRAL, AGREE or STRONGLY AGREE. The values in these columns represent PERCENTAGES of the total students who responded with that answer.
+
+The table nss has one row per institution, subject and question.
+
+1. Show the the percentage who STRONGLY AGREE.
+
+```sql
+SELECT A_STRONGLY_AGREE
+FROM nss
+WHERE question='Q01'
+	AND institution='Edinburgh Napier University'
+	AND subject='(8) Computer Science';
+```
+
+2. Show the institution and subject where the score is at least 100 for question 15.
+
+```sql
+SELECT institution, subject
+FROM nss
+WHERE question='Q15'
+	AND score >= 100;
+```
+
+3. Show the institution and score where the score for '(8) Computer Science' is less than 50 for question 'Q15'.
+
+```sql
+SELECT institution, score
+FROM nss
+WHERE question='Q15'
+	AND subject='(8) Computer Science'
+	AND score < 50;
+```
+
+4. Show the subject and total number of students who responded to question 22 for each of the subjects '(8) Computer Science' and '(H) Creative Arts and Design'.
+
+```sql
+SELECT subject, SUM(response)
+FROM nss
+WHERE question='Q22'
+	AND (subject='(8) Computer Science'
+	OR subject = '(H) Creative Arts and Design')
+GROUP BY subject;
+```
+
+5. Show the subject and total number of students who A_STRONGLY_AGREE to question 22 for each of the subjects '(8) Computer Science' and '(H) Creative Arts and Design'.
+
+```sql
+SELECT subject, SUM(A_STRONGLY_AGREE*response/100)
+FROM nss
+WHERE question='Q22'
+	AND (subject='(8) Computer Science'
+		OR subject='(H) Creative Arts and Design')
+GROUP BY subject;
+```
+
+6. Show the percentage of students who A_STRONGLY_AGREE to question 22 for the subject '(8) Computer Science' show the same figure for the subject '(H) Creative Arts and Design'.
+
+```sql
+SELECT subject, ROUND(SUM(response*A_STRONGLY_AGREE/100) / SUM(response) * 100)
+FROM nss
+WHERE question='Q22'
+	AND (subject='(8) Computer Science'
+		OR subject='(H) Creative Arts and Design')
+GROUP BY subject;
+```
+
+7. Show the average scores for question 'Q22' for each institution that include 'Manchester' in the name.
+
+```sql
+SELECT institution, ROUND(SUM(response*score) / SUM(response))
+FROM nss
+WHERE question='Q22'
+	AND (institution LIKE '%Manchester%')
+GROUP BY institution;
+```
+
+8. Show the institution, the total sample size and the number of computing students for institutions in Manchester for 'Q01'.
+
+```sql
+SELECT institution, SUM(sample), SUM(CASE WHEN subject='(8) Computer Science' THEN sample END)
+FROM nss
+WHERE question='Q01'
+	AND (institution LIKE '%Manchester%')
+GROUP BY institution;
+```
--- solutionsAll.sql
+++ solutionsAll.sql
@@ -607,7 +607,7 @@ SELECT SUM(population) FROM bbc WHERE region = 'Europe'
 SELECT COUNT(name) FROM bbc WHERE population < 150000
 
 -- 3. Select the list of core SQL aggregate functions.
-AVG(), COUNT(), MAX(), MIN(), SUM()
+-- AVG(), COUNT(), MAX(), MIN(), SUM()
 
 -- 4. Select the result that would be obtained from the following code:
 SELECT region, SUM(area)
@@ -728,7 +728,7 @@ ORDER BY mdate, matchid, team1, team2;
 
 -- JOIN quiz (using same tables as exercises)
 -- 1. You want to find the stadium where player 'Dimitris Salpingidis' scored. Select the JOIN condition to use.
-game JOIN goal ON (id=matchid)
+-- game JOIN goal ON (id=matchid)
 
 -- 2. You JOIN the tables goal and eteam in an SQL statement. Indicate the list of column names that may be used in the SELECT line.
 matchid, teamid, player, gtime, id, teamname, coach
@@ -1049,3 +1049,71 @@ SELECT name,
 	END 
 FROM teacher
 -- Table-A: Computing (Shrivell, Throd, Split), Other (Spiregrain, Cutflower, Deadyawn)
+
+----------
+
+-- 8+. NSS Tutorial (Numeric Examples)
+
+-- National Student Survey 2012
+
+-- The National Student Survey http://www.thestudentsurvey.com/ is presented to thousands of graduating students in UK Higher Education. The survey asks 22 questions, students can respond with STRONGLY DISAGREE, DISAGREE, NEUTRAL, AGREE or STRONGLY AGREE. The values in these columns represent PERCENTAGES of the total students who responded with that answer.
+
+-- The table nss has one row per institution, subject and question.
+
+-- 1. Show the the percentage who STRONGLY AGREE.
+SELECT A_STRONGLY_AGREE
+FROM nss
+WHERE question='Q01'
+	AND institution='Edinburgh Napier University'
+	AND subject='(8) Computer Science';
+
+-- 2. Show the institution and subject where the score is at least 100 for question 15.
+SELECT institution, subject
+FROM nss
+WHERE question='Q15'
+	AND score >= 100;
+
+-- 3. Show the institution and score where the score for '(8) Computer Science' is less than 50 for question 'Q15'.
+SELECT institution, score
+FROM nss
+WHERE question='Q15'
+	AND subject='(8) Computer Science'
+	AND score < 50;
+
+-- 4. Show the subject and total number of students who responded to question 22 for each of the subjects '(8) Computer Science' and '(H) Creative Arts and Design'.
+SELECT subject, SUM(response)
+FROM nss
+WHERE question='Q22'
+	AND (subject='(8) Computer Science'
+	OR subject = '(H) Creative Arts and Design')
+GROUP BY subject;
+
+-- 5. Show the subject and total number of students who A_STRONGLY_AGREE to question 22 for each of the subjects '(8) Computer Science' and '(H) Creative Arts and Design'.
+SELECT subject, SUM(A_STRONGLY_AGREE*response/100)
+FROM nss
+WHERE question='Q22'
+	AND (subject='(8) Computer Science'
+		OR subject='(H) Creative Arts and Design')
+GROUP BY subject;
+
+-- 6. Show the percentage of students who A_STRONGLY_AGREE to question 22 for the subject '(8) Computer Science' show the same figure for the subject '(H) Creative Arts and Design'.
+SELECT subject, ROUND(SUM(response*A_STRONGLY_AGREE/100) / SUM(response) * 100)
+FROM nss
+WHERE question='Q22'
+	AND (subject='(8) Computer Science'
+		OR subject='(H) Creative Arts and Design')
+GROUP BY subject;
+
+-- 7. Show the average scores for question 'Q22' for each institution that include 'Manchester' in the name.
+SELECT institution, ROUND(SUM(response*score) / SUM(response))
+FROM nss
+WHERE question='Q22'
+	AND (institution LIKE '%Manchester%')
+GROUP BY institution;
+
+-- 8. Show the institution, the total sample size and the number of computing students for institutions in Manchester for 'Q01'.
+SELECT institution, SUM(sample), SUM(CASE WHEN subject='(8) Computer Science' THEN sample END)
+FROM nss
+WHERE question='Q01'
+	AND (institution LIKE '%Manchester%')
+GROUP BY institution;
